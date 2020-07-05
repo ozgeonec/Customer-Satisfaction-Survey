98a5e28c705c6c162057a3aaf31a88523b3aca83
ozgeonec <53258721+ozgeonec@users.noreply.github.com>
2020-07-05 12:40:37 +0300

    dependency check

---
+++ src/main/java/com/example/customersurvey/CustomersurveyApplication.java
@@ -0,0 +1,13 @@
+package com.example.customersurvey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomersurveyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomersurveyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/customersurvey/CustomersurveyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.customersurvey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomersurveyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
